0e9323e8b2d06a21012e5065ae521b4e703e9823
vkrasnovyd <vkrasnovyd@proton.me>
2025-08-09 16:40:35 +0200

    Change clock visibility on mouse click

--- src/App.tsx
+++ src/App.tsx
@@ -11,62 +11,94 @@ function getRandomName(): string {
 type State = {
   today: Date;
   clockName: string;
-  timerId: number;
+  clockNameTimerId: number;
+  todayTimerId: number;
+  hasClock: boolean;
 };
 
 export class App extends React.Component<{}, State> {
   state: State = {
     today: new Date(),
     clockName: 'Clock-0',
-    timerId: 0,
+    clockNameTimerId: 0,
+    todayTimerId: 0,
+    hasClock: true,
+  };
+
+  hideClock = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  };
+
+  showClock = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: true });
   };
 
   // This code starts a timer
   componentDidMount(): void {
+    const currentTime = new Date();
+
     this.setState({
-      timerId: window.setInterval(
+      today: currentTime,
+      clockNameTimerId: window.setInterval(
         () => this.setState({ clockName: getRandomName() }),
         3300,
       ),
+      todayTimerId: window.setInterval(
+        () => this.setState({ today: new Date() }),
+        1000,
+      ),
     });
 
-    window.setInterval(() => this.setState({ today: new Date() }), 1000);
+    document.addEventListener('click', this.showClock);
+    document.addEventListener('contextmenu', this.hideClock);
   }
 
-  componentDidUpdate(prevState: Readonly<State>): void {
-    if (prevState.today !== this.state.today) {
-      console.log(this.state.today.toUTCString().slice(-12, -4));
-    }
+  componentDidUpdate(
+    prevProps: Readonly<{}>,
+    prevState: Readonly<State>,
+  ): void {
+    if (this.state.hasClock) {
+      if (prevState.today !== this.state.today) {
+        console.log(this.state.today.toUTCString().slice(-12, -4));
+      }
 
-    if (prevState.clockName !== this.state.clockName) {
-      console.warn(
-        `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
-      );
+      if (prevState.clockName !== this.state.clockName) {
+        console.warn(
+          `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
+        );
+      }
     }
   }
 
   // this code stops the timer
   componentWillUnmount(): void {
-    window.clearInterval(this.state.timerId);
-    this.setState({ timerId: 0 });
+    window.clearInterval(this.state.clockNameTimerId);
+    window.clearInterval(this.state.todayTimerId);
+
+    document.removeEventListener('click', this.showClock);
+    document.removeEventListener('contextmenu', this.hideClock);
   }
 
   render() {
-    const { today, clockName } = this.state;
+    const { today, clockName, hasClock } = this.state;
 
     return (
       <div className="App">
         <h1>React clock</h1>
 
-        <div className="Clock">
-          <strong className="Clock__name">{clockName}</strong>
+        {hasClock && (
+          <div className="Clock">
+            <strong className="Clock__name">{clockName}</strong>
 
-          {' time is '}
+            {' time is '}
 
-          <span className="Clock__time">
-            {today.toUTCString().slice(-12, -4)}
-          </span>
-        </div>
+            <span className="Clock__time">
+              {today.toUTCString().slice(-12, -4)}
+            </span>
+          </div>
+        )}
       </div>
     );
   }
